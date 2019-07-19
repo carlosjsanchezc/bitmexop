ef8a62644fc9cb4530003b87bd8f5f9e47044cce
carlosjsanchezc <carlosjsanchezc@gmail.com>
2019-07-19 09:36:00 -0500

    19072019

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { BitmexService } from './../bitmex.service';
 import { RealtimeService } from './../realtime.service';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { isUndefined } from 'util';
 
 @Component({
   selector: 'app-tab1',
@@ -13,9 +14,46 @@ export class Tab1Page {
   symbol:string="XBTUSD";
   type:string="Market";
   price:number=0;
+  xbtusd:number=0;
+  ethusd:number=0;
+  ltcu19:number=0;
+  xrpu19:number=0;
   quantity:number=1000;
+  wsurl:string="wss://www.bitmex.com/realtime?subscribe=instrument";
   leverage:number=5;
   constructor(private http: HttpClient, private bitmex: BitmexService) {
+    var ws=new WebSocket(this.wsurl);
+    let xbtusd=0;
+    ws.onmessage=(e)=> {
+      //console.log('Server: ' + e.data);
+      let dataserver=e.data;
+      let d:string=dataserver;
+      
+      if (d.indexOf("lastPrice")>0)
+      {
+        let objData=JSON.parse(dataserver);
+        if (objData.data[0].symbol=="XBTUSD"){
+          if (!isUndefined(objData.data[0].lastPrice)){
+            this.xbtusd=objData.data[0].lastPrice;
+          }
+        }
+        if (objData.data[0].symbol=="ETHUSD"){
+          if (!isUndefined(objData.data[0].lastPrice)){
+            this.ethusd=objData.data[0].lastPrice;
+          }
+        }
+        if (objData.data[0].symbol=="LTCU19"){
+          if (!isUndefined(objData.data[0].lastPrice)){
+            this.ltcu19=objData.data[0].lastPrice;
+          }
+        }
+        if (objData.data[0].symbol=="XRPU19"){
+          if (!isUndefined(objData.data[0].lastPrice)){
+            this.xrpu19=objData.data[0].lastPrice;
+          }
+        }
+      }
+    };
 
   }
   Buy() {
